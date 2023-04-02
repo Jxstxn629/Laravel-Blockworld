4636785dd6044e3219ec8fb132b29b15736116ea
Jxstxn629 <justin_ng_jt@hotmail.com>
2023-04-03 01:23:37 +0800

    install npm and bootstrap

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+   
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <h1>Hello World</h1>
+</body>
+</html>
